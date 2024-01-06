b3427ca1866161af34efb55a2e41a53b900f9963
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2024-01-06 21:02:08 +0900

    Trash 수정 중

--- HungryWhale/Assets/01_Scripts/Obstacle/Trash.cs
+++ HungryWhale/Assets/01_Scripts/Obstacle/Trash.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Trash : MonoBehaviour
 {
+    [Header("¿¡¼Â")]
+    public List<Sprite> sprites = new List<Sprite>();
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
