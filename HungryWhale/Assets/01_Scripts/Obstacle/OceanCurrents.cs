fcbf7779823621f3c1a21016a65cd284762a8de2
minchae123 <08minchae17@gmail.com>
2024-01-07 04:17:21 +0900

    ++

--- HungryWhale/Assets/01_Scripts/Obstacle/OceanCurrents.cs
+++ HungryWhale/Assets/01_Scripts/Obstacle/OceanCurrents.cs
@@ -8,6 +8,9 @@ public class OceanCurrents : MonoBehaviour
     private GameObject player;
     private bool isOceanCurrents;
 
+	[SerializeField] private ParticleSystem waveParticleL;
+	[SerializeField] private ParticleSystem waveParticleR;
+
     private void Start()
     {
         player = GameObject.Find("Player");
@@ -16,9 +19,35 @@ public class OceanCurrents : MonoBehaviour
 
     IEnumerator ActiveOceanCurrents()
     {
-        yield return new WaitForSeconds(20);
-        int rand = Random.Range(0, 2) == 0 ? 1 : -1;
-        player.GetComponent<Rigidbody2D>().AddForce(new Vector3(rand * 500, 0, 0));
-        StartCoroutine(ActiveOceanCurrents());
+        while(true)
+		{
+            yield return new WaitForSeconds(10);
+            int rand = Random.Range(0, 2) == 0 ? 1 : -1;
+            StartCoroutine(PlayParticle(rand, 10));
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector3(rand * 500, 0, 0));
+            yield return new WaitForSeconds(10);
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector3(0, 0, 0));
+		}
     }
+
+    IEnumerator PlayParticle(int i, int time)
+	{
+        if(i == -1)
+		{
+            waveParticleL.gameObject.SetActive(true);
+            waveParticleL.Play();
+            yield return new WaitForSeconds(time);
+            waveParticleL.Stop();
+            waveParticleL.gameObject.SetActive(false);
+		}
+		else if(i == 1)
+		{
+            waveParticleR.gameObject.SetActive(true);
+            waveParticleR.Play();
+            yield return new WaitForSeconds(time);
+            waveParticleR.Stop();
+            waveParticleR.gameObject.SetActive(false);
+        }
+
+	}
 }
