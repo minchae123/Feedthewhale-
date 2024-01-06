7148f9e49f007bcdf9eba14619192f3276de57e1
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2024-01-06 23:42:40 +0900

    TrashSpawner

--- HungryWhale/Assets/01_Scripts/Obstacle/Trash.cs
+++ HungryWhale/Assets/01_Scripts/Obstacle/Trash.cs
@@ -4,14 +4,53 @@ using UnityEngine;
 
 public class Trash : MonoBehaviour
 {
-    [Header("에셋")]
-    public List<Sprite> sprites = new List<Sprite>();
+    private GameObject player;
+    private float speed = 5;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public float floatSpeed = 1.0f; // 둥둥 떠다니는 속도
+    public float floatHeight = 1.0f; // 둥둥 높이
+
+    private Vector2 startPosition;
+
+    private void Start()
     {
-        if (collision.gameObject.tag == "Player")
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+        Check();
+    }
+
+    public void SetPlayer(GameObject p)
+    {
+        player = p;
+    }
+
+    private void Move()
+    {
+        Vector2 currentPosition = transform.position;
+
+        // 둥둥 떠다니는 속도를 더해 새로운 위치를 계산
+        currentPosition.y = startPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+
+        // 새로운 위치로 업데이트
+        transform.position = currentPosition;
+    }
+
+    private void Check()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) > 18)
         {
-            collision.gameObject.GetComponent<Player>().DecreaseHP(5);
+            TrashSpawner.instance.spawnCount--;
+            Destroy(gameObject);
         }
+    }
+
+    public void Eaten()
+    {
+        Destroy(gameObject);
+        player.gameObject.GetComponent<Player>().DecreaseHP(5);
     }
 }
