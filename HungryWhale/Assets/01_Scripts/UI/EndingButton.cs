22e0e2df35703fafe0483ac1aeeaabaa8a0a0e63
didtmdrnjs <105967323+didtmdrnjs@users.noreply.github.com>
2024-01-07 06:33:04 +0900

    엔딩

---
+++ HungryWhale/Assets/01_Scripts/UI/EndingButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingButton : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ReStart()
+    {
+        SceneManager.LoadScene("");
+    }
+}
