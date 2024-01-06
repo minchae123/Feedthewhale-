09bcee9dbab82469b17f624991b8bd64fa44a0c7
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2024-01-06 17:58:59 +0900

    씨네머신

--- HungryWhale/Assets/01_Scripts/Obstacle/Trash.cs
+++ HungryWhale/Assets/01_Scripts/Obstacle/Trash.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Trash : MonoBehaviour
 {
+    private void Update()
+    {
+        
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
