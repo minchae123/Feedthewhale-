33b6c4eb5a74e60a47c6167d10c7eae888c6a9b0
didtmdrnjs <105967323+didtmdrnjs@users.noreply.github.com>
2024-01-06 22:08:33 +0900

    fish

--- HungryWhale/Assets/01_Scripts/SmallFish/FishSpawner.cs
+++ HungryWhale/Assets/01_Scripts/SmallFish/FishSpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FishSpawner : MonoBehaviour
 {
     public static FishSpawner instance { get; private set; }
-    public GameObject smallFish;
+    public List<GameObject> smallFish = new();
     public int spawnCount = 0;
 
     private GameObject player;
@@ -27,7 +27,7 @@ public class FishSpawner : MonoBehaviour
     {
         if (spawnCount < maxSpawn)
         {
-            GameObject fish = Instantiate(smallFish, new Vector3(player.transform.position.x + Random.Range(-20f, 20f), Random.Range(-2.6f, 10f), 0), Quaternion.identity);
+            GameObject fish = Instantiate(smallFish[Random.Range(0, smallFish.Count)], new Vector3(player.transform.position.x + Random.Range(-20f, 20f), Random.Range(-2.6f, 10f), 0), Quaternion.identity);
             fish.GetComponent<Fish>().SetPlayer(player);
             int dir = Random.Range(1, 3);
             if (dir == 1) fish.GetComponent<SpriteRenderer>().flipX = true;
