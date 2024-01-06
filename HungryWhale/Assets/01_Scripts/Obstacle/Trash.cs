618a90105139c4ea1a53aed2fdf11d65170abd9d
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2024-01-06 16:48:03 +0900

    Trash

---
+++ HungryWhale/Assets/01_Scripts/Obstacle/Trash.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Player>().DecreaseHP(5);
+        }
+    }
+}
